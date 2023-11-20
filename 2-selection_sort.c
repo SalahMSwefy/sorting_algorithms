b7c918ddfd0ce909edc1bf22b21845d020eeae44
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-11-20 19:05:47 +0200

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,60 +1,30 @@
 #include "sort.h"
 
 /**
- * swap - swaps 2 values
- * @a: the first int
- * @b: the seconde int
- * Reuturn: void
-*/
-void swap(int *a, int *b)
-{
-	int temp;
+ * selection_sort - sorting array using selection sort algorithm
+ * @array: the array of ints
+ * @size: size of the array
+ */
 
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-/**
- * get_min_index - gets the minimum index for a sub array
- * @array: the array to sort
- * @start: the starting index of the sub array
- * @size: the size of the array
- * Return: min_index
-*/
-size_t get_min_index(int *array, int start, size_t size)
-{
-	size_t i, min_index;
-	int min_value;
-
-	min_value = array[start];
-	min_index = start;
-	for (i = start + 1; i < size; i++)
-	{
-		if (array[i] < min_value)
-		{
-			min_value = array[i];
-			min_index = i;
-		}
-	}
-	return (min_index);
-}
-/**
- * selection_sort - sorts an array using selection sort algorithm
- * @array: the array to sort
- * @size: the size of the array
- * Return: void
-*/
 void selection_sort(int *array, size_t size)
 {
-	size_t i, min_index;
+	size_t i, j, min, tmp;
+
+	if (size < 2)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
-		min_index = get_min_index(array, i, size);
-		if (i != min_index)
+		min = i;
+		for (j = i + 1; j < size; j++)
 		{
-			swap(&array[i], &array[min_index]);
-			print_array(array, size);
+			if (array[j] < array[min])
+				min = j;
 		}
+		tmp = array[i];
+		array[i] = array[min];
+		array[min] = tmp;
+		if (i != min)
+			print_array(array, size);
 	}
 }
